badf770e453090c36b01a842ad34ea385fd26424
HCasalderrey <el_topo_48@hotmail.com>
2024-02-23 17:18:19 -0300

    page index and others page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
+import animations from '@midudev/tailwind-animations'
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    plugins: [animations],
   integrations: [tailwind()]
 });
