2cc3a6ead8d69ab7d3792517df17d6ac3c9cb116
HCasalderrey <el_topo_48@hotmail.com>
2024-03-21 11:20:28 -0300

    add .env and fix button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,8 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+         
+      
 		extend: {},
 	},
 	plugins: [animations],
